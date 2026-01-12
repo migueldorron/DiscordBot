91af0eefb36041d7380ed2486141d52056d6bcdb
DorronSmogon <mdorronsoromontero@gmail.com>
2026-01-12 22:36:22 +0100

    SSB check

--- cogs/SSB.py
+++ cogs/SSB.py
@@ -1,45 +1,36 @@
 import discord
+import databases.pokesSSB
 from discord.ext import commands
 import os
 
 
-#Different commands I tried using while I was learning both Python and the Discord library. Uploaded just for information purposes.
-class MiscelaneoCog(commands.Cog):
+class SSBCog(commands.Cog):
+    listaPokemon=databases.pokesSSB.lista
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def verificar(self, ctx, input_pokemon, df):
+    async def puntos(self, ctx, *, input_pokemon):
         """
         Verifica la validez de un equipo de 6 Pokémon ingresados como un único string.
         Calcula la suma de puntos del equipo.
         """
-        # Dividir el input por el separador " / "
         nombres = input_pokemon.split(" / ")
 
         if len(nombres) != 6:
             await ctx.send("Copiaste mal el team preview")
+            return
 
-        # Buscar los Pokémon en el DataFrame
-        equipo = df[df["Pokémon"].isin(nombres)]
-
-        if len(equipo) != 6:
-            return "Copiaste mal el team preview"
-
-        # Calcular la suma de puntos
-        suma_puntos = equipo["puntos"].sum()
-
-        # Mostrar los detalles del equipo y la suma de puntos
-        print("Equipo ingresado:")
-        print(equipo[["Pokémon", "Rank", "puntos"]].to_string(index=False))
-        return f"Suma total de puntos: {suma_puntos}"
+        sumaEquipo=0
+        for pokemon in nombres:
+            if pokemon in self.listaPokemon:
+                sumaEquipo+=self.listaPokemon[pokemon][1]
+            else:
+                await ctx.send("Uno de los Pokemon no existe. Revisa si has copiado bien el team preview.")
 
 
 
-    print("Ingresa los pokes como salen en el team preview\nEjemplo:\nGhostGius's team: \nTapu Koko / Garganacl / Tinkaton / Ursaluna-Bloodmoon / Marshadow / Eelektross-Mega")
-    input_pokemon = input("Pega el team preview: \n")
-    resultado = verificar_equipo(input_pokemon, dfrb)
-    print(resultado)
+        await ctx.send(f"Equipo mandado: {input_pokemon} - Suma de puntos: {sumaEquipo}")
 
 async def setup(bot):
-    await bot.add_cog(MiscelaneoCog(bot))
+    await bot.add_cog(SSBCog(bot))
