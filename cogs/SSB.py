b469ccb98ed7661b7a40c40b2984f32329f820b1
DorronSmogon <mdorronsoromontero@gmail.com>
2026-01-13 15:24:08 +0100

    SSB tiers check

--- cogs/SSB.py
+++ cogs/SSB.py
@@ -4,16 +4,14 @@ from discord.ext import commands
 
 
 class SSBCog(commands.Cog):
-    listaPokemon=databases.pokesSSB.lista
+    listaPokemon=databases.pokesSSB.listaPokemon
+    formatos=databases.pokesSSB.formatos
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def puntos(self, ctx, *, input_pokemon):
-        """
-        Verifica la validez de un equipo de 6 Pokémon ingresados como un único string.
-        Calcula la suma de puntos del equipo.
-        """
+
         nombres = input_pokemon.split(" / ")
 
         if len(nombres) > 6:
@@ -30,7 +28,52 @@ class SSBCog(commands.Cog):
                 await ctx.send("Uno de los Pokemon no existe. Revisa si has copiado bien el team preview.")
                 return
 
-        await ctx.send(f"Equipo mandado: {input_pokemon} \nRangos: {rangosEquipo} \nSuma de puntos: {sumaEquipo}")
+        await ctx.send(f"Rangos: {rangosEquipo} \nSuma de puntos: {sumaEquipo}")
+
+    @commands.command()
+    async def tiers(self, ctx, *, input_pokemon):
+
+        nombres = input_pokemon.split(" / ")
+
+        if len(nombres) > 6:
+            await ctx.send("Copiaste mal el team preview. Demasiados elementos.")
+            return
+
+        sumaEquipo=0
+        rangosEquipo=""
+        formatosPoke={}
+        for pokemon in nombres:
+            if pokemon in self.listaPokemon:
+                sumaEquipo+=self.listaPokemon[pokemon][1]
+                rangosEquipo+=self.listaPokemon[pokemon][0]+" "
+                formatosPokeTemporal=self.listaPokemon[pokemon][2:]
+                formatosPoke[pokemon]=formatosPokeTemporal
+            else:
+                await ctx.send("Uno de los Pokemon no existe. Revisa si has copiado bien el team preview.")
+                return
+
+        formatosValidos=[True] * 8
+
+        for i in range(8):
+            for poke in formatosPoke:
+                if not formatosPoke[poke][i]:
+                    formatosValidos[i]=False
+                    break
+
+        listaFormatosValidos=[]
+        for i, formato in enumerate(formatosValidos):
+            if formato:
+                listaFormatosValidos.append(self.formatos[i])
+
+        if listaFormatosValidos:
+            await ctx.send(f"""Rangos: {rangosEquipo} \n
+                           Suma de puntos: {sumaEquipo} \n
+                           Tiers en las que se permite tu equipo: {listaFormatosValidos}""")
+        else:
+            await ctx.send("Tu equipo no es válido en ninguna tier lol.")
+        
+
+
 
 async def setup(bot):
     await bot.add_cog(SSBCog(bot))
