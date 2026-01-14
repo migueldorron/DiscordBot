a260a503b399bd00223d28b445409be05d85f0d7
DorronSmogon <mdorronsoromontero@gmail.com>
2026-01-14 21:46:33 +0100

    Moderation file and ban command

---
+++ cogs/mod.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+from datetime import date
+import unicodedata
+import json
+from dotenv import load_dotenv
+import os
+import io
+
+load_dotenv()
+backup = int(os.getenv("SEND_ID"))
+
+#Different commands I tried using while I was learning both Python and the Discord library. Uploaded just for information purposes.
+class ModCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command()
+    async def banid(self, ctx, user_id: int, *, razon: str = "Unspecified reason"):
+        try:
+            if ctx.author.id!=438078850140864532:
+                await ctx.send("You can't use that command")
+                return
+            
+            user = await self.bot.fetch_user(user_id)
+            await ctx.guild.ban(user, reason=razon)
+
+        except discord.NotFound:
+            await ctx.send("User not found.")
+        except discord.Forbidden:
+            await ctx.send("I have no perms to ban that user..")
+        except Exception as e:
+            await ctx.send(f"Something unexpected happened: {e}")
+        
+async def setup(bot):
+    await bot.add_cog(ModCog(bot))
